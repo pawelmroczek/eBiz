f6b3c0055b52cab1135fada274cef9ee94e2c820
Vincenzo0611 <vincenzo.piras@o2.pl>
2025-01-18 18:34:48 +0100

    cache

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,12 @@ COPY ./config_ssl/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
 
 COPY start_script.sh /usr/local/bin/start_script.sh
 RUN chmod +x /usr/local/bin/start_script.sh
-RUN chmod 777 -R ./
+#RUN chmod 777 -R ./
+
+RUN apt-get update && apt-get install -y \
+    libmemcached-dev \
+    zlib1g-dev \
+    && pecl install memcached \
+    && docker-php-ext-enable memcached
 
 CMD ["sh", "/usr/local/bin/start_script.sh"]
---
+++ src/psdata/memcached-3.1.5/config.h
@@ -0,0 +1,86 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Whether to build memcached as dynamic module */
+#define COMPILE_DL_MEMCACHED 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <fastlz.h> header file. */
+/* #undef HAVE_FASTLZ_H */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Whether JSON API is available */
+/* #undef HAVE_JSON_API */
+
+/* Whether memcached_exist is defined */
+#define HAVE_MEMCACHED_EXIST 1
+
+/* Whether memcache igbinary serializer is enabled */
+/* #undef HAVE_MEMCACHED_IGBINARY */
+
+/* Whether memcache msgpack serializer is enabled */
+/* #undef HAVE_MEMCACHED_MSGPACK */
+
+/* Whether memcached protocol is enabled */
+/* #undef HAVE_MEMCACHED_PROTOCOL */
+
+/* Have SASL support */
+#define HAVE_MEMCACHED_SASL 1
+
+/* Whether memcache session handler is enabled */
+#define HAVE_MEMCACHED_SESSION 1
+
+/* Whether memcached_set_encoding_key is defined */
+#define HAVE_MEMCACHED_SET_ENCODING_KEY 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <sasl/sasl.h> header file. */
+#define HAVE_SASL_SASL_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+/* #undef PACKAGE_BUGREPORT */
+
+/* Define to the full name of this package. */
+/* #undef PACKAGE_NAME */
+
+/* Define to the full name and version of this package. */
+/* #undef PACKAGE_STRING */
+
+/* Define to the one symbol short name of this package. */
+/* #undef PACKAGE_TARNAME */
+
+/* Define to the home page for this package. */
+/* #undef PACKAGE_URL */
+
+/* Define to the version of this package. */
+/* #undef PACKAGE_VERSION */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
